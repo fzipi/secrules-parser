4f33962c1490c862a71071578b1cd5b0a7e70f1b
Felipe Zipitría <fzipi@fing.edu.uy>
2017-09-29 16:29:53 -0300

    added parsing multiple conf files

--- modsec.py
+++ modsec.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
+import os
+import glob
+import pprint
 from textx.metamodel import metamodel_from_file
+
 modsec_mm = metamodel_from_file('modsec.tx')
 
-modsec_model = modsec_mm.model_from_file('test/rules.conf')
+for rules in glob.glob('test/*.conf'):
+    print 'Processing %s' % rules
+    modsec_model = modsec_mm.model_from_file(rules)
+    pp = pprint.PrettyPrinter(indent=4,depth=6)
+    pp.pprint(modsec_model)
