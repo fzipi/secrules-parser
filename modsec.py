eb4a067ff83a01a8cf59fa9468483b1a7fe0746a
Felipe Zipitría <fzipi@fing.edu.uy>
2017-10-02 20:38:21 -0300

    print simple ids

--- modsec.py
+++ modsec.py
@@ -6,13 +6,21 @@ import pprint
 from textx.metamodel import metamodel_from_file
 from textx.exceptions import TextXSyntaxError
 
+def print_rule(rule):
+    if rule.__class__.__name__ == "SecRule":
+        for action in rule.actions.action:
+            if action.id:
+                print "Rule id = {}".format(action.id)
+            if action.chain:
+                print "Rule id = {} is a chained rule.".format(action.id)
+
 modsec_mm = metamodel_from_file('modsec.tx')
 
 for rules in glob.glob('owasp-modsecurity-crs/rules/*.conf'):
     print 'Processing file %s:' % rules
     try:
-        modsec_model = modsec_mm.model_from_file(rules)
-        pp = pprint.PrettyPrinter(indent=4,depth=6)
-        pp.pprint(modsec_model)
+        model = modsec_mm.model_from_file(rules)
+        for rule in model.rules:
+            print_rule(rule)
     except TextXSyntaxError as e:
         print e
