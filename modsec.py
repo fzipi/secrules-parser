c5d3d96b4dc65d4d8965eef63f418a9b81420fff
Felipe Zipitría <fzipi@fing.edu.uy>
2017-10-03 19:09:21 -0300

    added verbosing to execution, and parameters for parsing files

--- modsec.py
+++ modsec.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 
+""""
+ModSecurity CRS Parser 
+-----------------------------------------------
+
+Copyright (C) 2017 Felipe Zipitria <felipe.zipitria@owasp.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License, either
+version 3, or (at your option), any later version.
+
+"""
+
 import os
 import glob
 import pprint
+import argparse
 from textx.metamodel import metamodel_from_file
 from textx.exceptions import TextXSyntaxError
+
+# Parse Arguments
+cmdline = argparse.ArgumentParser(description='ModSecurity CRS parser script.')
+cmdline.add_argument('-v', '--verbose', help='Print verbose messages', action="store_true")
+cmdline.add_argument('-f', '--files', type=str, help='Parse these files')
+
+args = cmdline.parse_args()
 
 def print_rule(rule):
     if rule.__class__.__name__ == "SecRule":
@@ -14,13 +34,24 @@ def print_rule(rule):
             if action.chain:
                 print "> Rule is a chained rule.".format(action.id)
 
-modsec_mm = metamodel_from_file('modsec.tx')
+modsec_mm = metamodel_from_file('modsec.tx', memoization=True)
 
-for rules in glob.glob('owasp-modsecurity-crs/rules/*.conf'):
-    print 'Processing file %s:' % rules
+# files is a glob pattern
+files = args.files if args.files else 'owasp-modsecurity-crs/rules/*.conf'
+
+for rules in glob.glob(files):
+    if args.verbose:
+        print 'Processing file %s:' % rules
     try:
         model = modsec_mm.model_from_file(rules)
-        for rule in model.rules:
-            print_rule(rule)
+        if args.verbose:
+            for rule in model.rules:
+                print_rule(rule)
+        if model.rules:
+            print "Syntax OK: {}".format(rules)
     except TextXSyntaxError as e:
+        print "Syntax error in line {}, col {}: {}".format(e.line, e.col, e.message)
+    except TextXSemanticError as e:
         print e
+
+
