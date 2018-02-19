c8c0f8fd1931cf56b48514fc3f82d0711512e8be
Felipe Zipitría <fzipitria@perceptyx.com>
2018-02-19 08:54:44 -0300

    First pass

--- modsec.py
+++ modsec.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """"
-ModSecurity CRS Parser 
+ModSecurity CRS Parser
 -----------------------------------------------
 
 Copyright (C) 2017 Felipe Zipitria <felipe.zipitria@owasp.org>
@@ -27,13 +27,37 @@ cmdline = argparse.ArgumentParser(description='ModSecurity CRS parser script.')
 cmdline.add_argument('-v', '--verbose', help='Print verbose messages', action="store_true")
 cmdline.add_argument('-d', '--debug', help='You don\'t want to do this!', action="store_true")
 cmdline.add_argument('-f', '--files', type=str, help='Parse these files')
+cmdline.add_argument('-r', '--reorder', help='Reorder rules using CRS standard', action="store_true")
 
 args = cmdline.parse_args()
 
-def secrule_id_processor(rule):
-    """ Processor for each rule, if neeeded """
-    # print "secrule_id_processor_called"
-    # print(rule)
+
+class Rule(object):
+    """ Rules are:
+    - a list of variables,
+    - negated (bool)
+    - Operator
+    - a list of actions """
+    def __init__(self, variables, negated, count, operator, actions):
+        print "rule init called"
+        if count:
+            self.count = True
+        for var in variables:
+            print var
+        for action in actions:
+            print action
+            #if action.id:
+            #  self.id = action.id
+        self.operator = operator
+        self.variables = variables
+
+# Main
+def order_using_crs_standard(rule):
+    """ Order rule output using CRS standard """
+    #if rule.__class__.__name__ == "SecRule":
+    #    for action in rule.actions:
+    #        if action.id:
+    #            return action.id
 
 def debug_rule(rule):
     """ Don't use this unless you want to wait and read too much """
@@ -48,6 +72,10 @@ def get_rule_id(rule):
                 return action.id
     return 0
 
+def print_comment(comment):
+    """ Comments are discarded, so this method is irrelevant now """
+    print "Comment: ".format(comment)
+
 def print_rule(rule):
     """ This is just for printing something until we see the real capabilities of this parser """
     if rule.__class__.__name__ == "SecRule":
@@ -60,11 +88,38 @@ def print_rule(rule):
                 print "* Rule id = {}".format(action.id)
             if action.chain:
                 print "* > This is a chained rule from the above.".format(action.id)
+        if args.reorder:
+            self.order_using_crs_standard(rule)
+
+# rules_processors
+def secrule_id_processor(rule):
+    """ Processor for each rule, if neeeded """
+    # print "secrule_id_processor_called"
+    # print(rule)
+
+def variable_processor(var):
+    print(var)
+
+def operator_processor(op):
+    print(op)
+
+def action_processor(action):
+    print(action)
+def disruptive_action(da):
+    print "DisruptiveAction: {}".format(da)
 
 # Load Meta-Model
-modsec_mm = metamodel_from_file('modsec.tx', memoization=True)
+# auto_init_attributes=False
+modsec_mm = metamodel_from_file('modsec.tx', classes=[Rule])
 # Register test processor
-modsec_mm.register_obj_processors({'SecRule': secrule_id_processor})
+rules_processors = {
+    'SecRule': secrule_id_processor,
+    'Variable': variable_processor,
+    'Operator': operator_processor,
+    'Action': action_processor,
+    'DisruptiveAction': disruptive_action
+}
+modsec_mm.register_obj_processors(rules_processors)
 
 # files is a glob pattern
 files = args.files if args.files else 'owasp-modsecurity-crs/rules/*.conf'
@@ -77,6 +132,11 @@ for rules in glob.glob(files):
         if args.verbose:
             for rule in model.rules:
                 print_rule(rule)
+            for comment in model.comments:
+                print_comment(comment)
+        if args.reorder:
+            for rule in model.rules:
+                order_using_crs_standard(rule)
         if args.debug:
             for rule in model.rules:
                 debug_rule(rule)
@@ -84,5 +144,3 @@ for rules in glob.glob(files):
             print "Syntax OK: {}".format(rules)
     except TextXSyntaxError as e:
         print "Syntax error in line {}, col {}: {}".format(e.line, e.col, e.message)
-
-
